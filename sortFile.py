895208a187c10bd68254349bcdc5d8068b4aaa4d
Przemyslaw <przemyslaw.dekowski@gmail.com>
2019-12-31 15:01:14 +0100

    rename file -> file_name

--- sortFile.py
+++ sortFile.py
@@ -14,13 +14,13 @@ def is_hidden_file(file_name):
 def sort_file(directory_path):
     dir_content = listdir(directory_path)
     dir_content.remove(path.basename(__file__))
-    for file in dir_content:
-        extension = file.split('.')[-1]
+    for file_name in dir_content:
+        extension = file_name.split('.')[-1]
         extension_dir = path.join(directory_path, extension)
-        if not is_hidden_file(file):
+        if not is_hidden_file(file_name):
             if not path.isdir(extension_dir):
                 mkdir(extension_dir)
-            replace(path.join(directory_path, file), path.join(directory_path, extension, file))
+            replace(path.join(directory_path, file_name), path.join(directory_path, extension, file_name))
 
 
 if __name__ == '__main__':
